6cc6013604a97ad32b8e88c7af3e84de1c654e49
Tyrone <tyrone.saulon@hotmail.com>
2018-08-06 19:05:42 -0400

    Adding interfaces with optional properties

--- documentation/interfaces.js
+++ documentation/interfaces.js
@@ -1,4 +1,5 @@
-function interfaces() {
+function firstInterface() {
+    //  Interface defined in function parameter datatype declaration
     function printLabel(labellledObj) {
         console.log(labellledObj.label);
     }
@@ -8,4 +9,41 @@ function interfaces() {
     };
     printLabel(obj);
 }
-interfaces();
+firstInterface();
+function secondInterface() {
+    //  capture properties of argument objects where
+    //  identifiers are identical to interface properties
+    function printLabel(labelledObj) {
+        console.log(labelledObj.label);
+    }
+    var obj = {
+        size: 10,
+        label: "ayy lmao, why are there two labels?"
+    };
+    printLabel(obj);
+}
+secondInterface();
+function thirdInterface() {
+    function createSquare(config) {
+        var tmp = { color: "white", area: 100 };
+        if (config.color) {
+            tmp.color = config.color;
+        }
+        if (config.width) {
+            tmp.area = config.width * config.width;
+        }
+        return tmp;
+    }
+    var mySquare = createSquare({ color: "black" });
+    console.log(mySquare);
+}
+thirdInterface();
+/*
+ *  Interfaces
+ *  >   Virtually type checks without using any class instances
+ *  >   Can do custom datatyping on objects
+ *  >   Captures only properties identical to interface properties
+ *  >   Does not generate any source code
+ *
+ *
+ */ 
--- documentation/interfaces.ts
+++ documentation/interfaces.ts
@@ -1,6 +1,7 @@
 function firstInterface(){
 
-    function printLabel(labellledObj: {label: string}){
+    //  Interface defined in function parameter datatype declaration
+    function printLabel(labellledObj: {label: string}){ //  labelledObj of type Object with a string property
         console.log(labellledObj.label)
     }
 
@@ -16,13 +17,64 @@ firstInterface()
 
 function secondInterface(){
     
+    //  Interface defined here
     interface LabelledValue{
         label: string;
     }
 
+    //  capture properties of argument objects where
+    //  identifiers are identical to interface properties
     function printLabel(labelledObj: LabelledValue){
         console.log(labelledObj.label)
     }
+
+    let obj = {
+        size: 10,
+        label: "ayy lmao, why are there two labels?"
+    }
+
+    printLabel(obj)
 }
 
 secondInterface();
+
+function thirdInterface(){
+
+    //  Interface with optional properties
+    interface SquareConfig{
+        color?: string,
+        width?: number
+    }
+
+    function createSquare(config: SquareConfig): {color: string, area: number}{
+
+        let tmp = {color: "white", area: 100}
+
+        if(config.color){
+            tmp.color = config.color;
+        }
+
+        if(config.width){
+            tmp.area = config.width * config.width;
+        }
+
+        return tmp;
+    }
+
+    let mySquare = createSquare({color: "black"})
+
+    console.log(mySquare)
+}
+
+thirdInterface()
+
+
+/*
+ *  Interfaces
+ *  >   Virtually type checks without using any class instances
+ *  >   Can do custom datatyping on objects
+ *  >   Captures only properties identical to interface properties
+ *  >   Does not generate any source code
+ * 
+ * 
+ */
